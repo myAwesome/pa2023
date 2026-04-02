7b62773bfcd834625396a53ea22986d272b11f2d
anastasiya1155 <anastasiya1155@gmail.com>
2026-04-02 14:03:39 +0200

    fix: use express 5 compatible wildcard route for spa fallback

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
